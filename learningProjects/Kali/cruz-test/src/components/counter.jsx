38e2e59eb5afa02a2ed8631fe84b847dff31c1cd
Kalinda Fraser <klfraser@ucsc.edu>
2019-02-03 21:28:27 -0800

    added functional increment button

--- learningProjects/Kali/cruz-test/src/components/counter.jsx
+++ learningProjects/Kali/cruz-test/src/components/counter.jsx
@@ -5,13 +5,33 @@ class Counter extends Component {
     count: 0
   };
 
+  // constructor() {
+  //  super();
+  //  this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
