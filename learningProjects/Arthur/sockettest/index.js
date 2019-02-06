72e93d0814c37477371b5ef3390c19bc5bcf3111
arthur <ali64@ucsc.edu>
2019-02-05 22:28:33 -0800

    socket.io test app is now complete! Working texting app on local host.

---
+++ learningProjects/Arthur/sockettest/index.js
@@ -0,0 +1,23 @@
+// index.js that sets up application.
+
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+  socket.broadcast.emit("hi");
+});
+
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(3000, function() {
+  console.log("listening on *:3000, yay");
+});
