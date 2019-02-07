0162f44fd4c7373d4b168f7204dafb11121834d3
arthur <ali64@ucsc.edu>
2019-02-06 22:58:43 -0800

    webApp folder updated --all

--- webApp/webApp/webApp/public/libs/report.js
+++
@@ -1,5 +0,0 @@
-/*
-    Standardized code to display all details of a given report number and 
-    of the mobile user who sent it in. Modal; activated through either 
-    dash.html or reports.html. 
-*/
