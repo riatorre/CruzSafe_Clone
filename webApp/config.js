80d71daef306bd1e56e6fc05ae9b122ceca1a796
arthur <ali64@ucsc.edu>
2019-02-06 23:12:46 -0800

    Everything works except ndoe start broke

--- webApp/config.js
+++ webApp/config.js
@@ -4,24 +4,24 @@
  * May need to change host in order for connection to work
  */
 
-var mysql = require('mysql');
-var myConsole = require('./customConsole');
+var mysql = require("mysql");
+var myConsole = require("./customConsole");
 
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: "localhost",
     port: 3306,
-    user: 'Compatibility_riatorre',
-    password: 'SiD1475357',
-    database: 'cruzsafe'
+    user: "Compatibility_riatorre",
+    password: "SiD1475357",
+    database: "cruzsafe"
 });
 
 connection.connect(err => {
     if (err) {
         myConsole.error(err);
-        myConsole.log('Closing Program');
-        process.exit(1);
+        myConsole.log("Closing Program");
+        //process.exit(1);
     } else {
-        myConsole.log('Database Connection Established');
+        myConsole.log("Database Connection Established");
     }
 });
 
