f608fedd0c03a345114740ffa01ef68d4aeb6eee
Youmna Shafiq <youmnashafiq@Youmnas-Air-2.attlocal.net>
2019-02-10 18:24:37 -0800

    edit additional Info

--- mobileApp/Richard/screens/AdditionalInfoScreen.js
+++ mobileApp/Richard/screens/AdditionalInfoScreen.js
@@ -13,88 +13,83 @@
 import React, { Component } from "react";
 import { Text, SafeAreaView, Platform } from "react-native";
 import {
-    Container,
-    Header,
-    Content,
-    Footer,
-    Left,
-    Right,
-    Body,
-    Icon
+  Container,
+  Header,
+  Content,
+  Footer,
+  Left,
+  Right,
+  Body,
+  Icon
 } from "native-base";
 
 import styles from "../components/styles.js";
 
 class AdditionalInfoScreen extends Component {
-    // Defining how the Drawer Button appears
-    static navigationOptions = {
-        // Drawer Name
-        drawerLabel: "Additional Info",
-        // Drawer Icon
-        drawerIcon: ({ tintColor }) => (
-            <Icon
-                name={`${Platform.OS === "ios" ? "ios" : "md"}-contact`}
-                style={{ fontSize: 24, color: tintColor }}
+  // Defining how the Drawer Button appears
+  static navigationOptions = {
+    // Drawer Name
+    drawerLabel: "More Info",
+    // Drawer Icon
+    drawerIcon: ({ tintColor }) => (
+      <Icon
+        name={`${Platform.OS === "ios" ? "ios" : "md"}-contact`}
+        style={{ fontSize: 24, color: tintColor }}
+      />
+    )
+  };
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <Container>
+          <Header style={styles.header}>
+            <Left>
+              {/* Icon used to open Side Drawer */}
+              <Icon
+                name={`${Platform.OS === "ios" ? "ios" : "md"}-menu`}
+                style={styles.icon}
+                onPress={() => this.props.navigation.openDrawer()}
+              />
+            </Left>
+            <Body>
+              {/* Center of Header */}
+              <Text style={styles.header_text}>More Info</Text>
+            </Body>
+            <Right />
+          </Header>
+          <Content contentContainerStyle={styles.container}>
+            {/* Body of Screen */}
+          </Content>
+
+          <Footer style={styles.footer}>
+            <Left
+              style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center"
+              }}
             />
-        )
-    };
-    render() {
-        return (
-            <SafeAreaView style={{ flex: 1 }}>
-                <Container>
-                    <Header style={styles.header}>
-                        <Left>
-                            {/* Icon used to open Side Drawer */}
-                            <Icon
-                                name={`${
-                                    Platform.OS === "ios" ? "ios" : "md"
-                                }-menu`}
-                                style={styles.icon}
-                                onPress={() =>
-                                    this.props.navigation.openDrawer()
-                                }
-                            />
-                        </Left>
-                        <Body>
-                            {/* Center of Header */}
-                            <Text style={styles.header_text}>
-                                Additional Info
-                            </Text>
-                        </Body>
-                        <Right />
-                    </Header>
-                    <Content contentContainerStyle={styles.container}>
-                        {/* Body of Screen */}
-                    </Content>
-                    <Footer style={styles.footer}>
-                        <Left
-                            style={{
-                                flex: 1,
-                                alignItems: "center",
-                                justifyContent: "center"
-                            }}
-                        />
-                        <Body
-                            style={{
-                                flex: 1,
-                                alignItems: "center",
-                                justifyContent: "center"
-                            }}
-                        >
-                            {/* Center of Footer */}
-                            <Text style={styles.footer_text}>CruzSafe</Text>
-                        </Body>
-                        <Right
-                            style={{
-                                flex: 1,
-                                alignItems: "center",
-                                justifyContent: "center"
-                            }}
-                        />
-                    </Footer>
-                </Container>
-            </SafeAreaView>
-        );
-    }
+            <Body
+              style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+            >
+              {/* Center of Footer */}
+              <Text style={styles.footer_text}>CruzSafe</Text>
+            </Body>
+            <Right
+              style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+            />
+          </Footer>
+        </Container>
+      </SafeAreaView>
+    );
+  }
 }
 export default AdditionalInfoScreen;
