b9d4d3ce8912744243f48dd47132fb0262573475
yshafiq <yshafiq@ucsc.edu>
2019-02-10 19:38:45 -0800

    updated MainTabNav after About US page

--- mobileApp/Richard/components/styles.js
+++ mobileApp/Richard/components/styles.js
@@ -6,56 +6,56 @@
 import { StatusBar, StyleSheet } from "react-native";
 
 export default (styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#CCC",
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    header: {
-        marginTop: StatusBar.currentHeight,
-        backgroundColor: "#336"
-    },
-    header_modal: {
-        backgroundColor: "#336"
-    },
-    footer: {
-        backgroundColor: "#336"
-    },
-    icon: {
-        color: "silver",
-        marginLeft: 10
-    },
-    header_text: {
-        color: "white",
-        fontSize: 20
-    },
-    footer_text: {
-        color: "white",
-        fontSize: 20
-    },
-    traffic_light: {
-        backgroundColor: "#333",
-        padding: 10
-    },
-    textInput: {
-        margin: 5,
-        padding: 5,
-        width: "80%",
-        backgroundColor: "#EEE",
-        borderColor: "gray",
-        borderWidth: 1,
-        marginTop: 8
-    },
-    picker: {
-        padding: 5
-    },
-    picker_view: {
-        margin: 5,
-        width: "80%",
-        backgroundColor: "#EEE",
-        borderColor: "gray",
-        borderWidth: 1,
-        marginTop: 8
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#CCC",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  header: {
+    marginTop: StatusBar.currentHeight,
+    backgroundColor: "#336"
+  },
+  header_modal: {
+    backgroundColor: "#336"
+  },
+  footer: {
+    backgroundColor: "#336"
+  },
+  icon: {
+    color: "silver",
+    marginLeft: 10
+  },
+  header_text: {
+    color: "white",
+    fontSize: 20
+  },
+  footer_text: {
+    color: "white",
+    fontSize: 20
+  },
+  traffic_light: {
+    backgroundColor: "#333",
+    padding: 10
+  },
+  textInput: {
+    margin: 5,
+    padding: 5,
+    width: "80%",
+    backgroundColor: "#EEE",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginTop: 8
+  },
+  picker: {
+    padding: 2
+  },
+  picker_view: {
+    margin: 5,
+    width: "80%",
+    backgroundColor: "#EEE",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginTop: 2
+  }
 }));
