95abfe115fbc7f9963d330e679b7d994480276c2
Richard Torres <rtorres513@hotmail.com>
2019-02-24 22:38:48 -0800

    webApp fixed

--- webApp/app.js
+++ webApp/app.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 const myConsole = require("./backend/utilities/customConsole");
 const users = require("./backend/routes/users");
 const reports = require("./backend/routes/reports");
-const public_router = require("./backend/routes/public");
+//const public_router = require("./backend/routes/public");
 
 const app = express();
 
@@ -26,7 +26,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Use the Router on the sub routes
 app.use("/api/users", users);
 app.use("/api/reports", reports);
-app.use("/", public_router);
+//app.use("/", public_router);
+
+app.get("/", function(req, res) {
+    res.sendFile("welcome.html", {
+        root: __dirname + "/public"
+    });
+});
+
+// Sets up Access Control for any page that is not the welcome page
+// Only affects html pages; should not affect APIs, images, css, js, etc pages/files
+// Uncomment out portion below in order to enable Authentication Verification
+//app.use("/*.html", verifyAuthentication);
 
 // Sets Default to public folder; allows for transfer of all files in public folder
 app.use(express.static(__dirname + "/public"));
@@ -38,4 +49,16 @@ if (module == require.main) {
         const port = server.address().port;
         myConsole.log("App listening on port " + port);
     });
+}
+
+// Access Control Function
+// Utilizes a function from Passport.js to determine if user is Authenticated
+function verifyAuthentication(req, res, next) {
+    if (req.isAuthenticated()) {
+        myConsole.log("User is Verified");
+        return next();
+    } else {
+        myConsole.log("User is Not Verified; redirecting to Welcome page");
+        res.redirect("/");
+    }
 }
