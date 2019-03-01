3923e45aa5a2d08380aa5fd8afecbba0b5f0cc8e
arthur <ali64@ucsc.edu>
2019-02-28 21:26:51 -0800

    Refactored report.js into three files.

---
+++ webApp/public/libs/reportListRefresher.js
@@ -0,0 +1,92 @@
+/*
+    Code meant specifically for reports.html. Allows for the refreshing of the page on a timed basis. 
+*/
+
+/*
+    Function that request the latest TS from database and sets that as latestTS hidden input in page.
+    Also takes in a firstRun; if 1, create a listener. else, don't.
+*/
+function getTS(document, hiddenID, firstRun) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            response = JSON.parse(request.response);
+            //console.log("getTS responded: " + JSON.stringify(response));
+            var maxTS = response[0]["MAX(reportTS)"];
+            console.log("getTS got maxTS of: " + maxTS);
+            var storedTS = document.getElementById(hiddenID);
+            var previouslyStored = storedTS.value;
+            storedTS.value = maxTS;
+            if (!firstRun) {
+                if (previouslyStored != maxTS) {
+                    getReportByTS(document, maxTS);
+                    determineSetup();
+                }
+            }
+        }
+    };
+    request.open("POST", "https://cruzsafe.appspot.com/api/reports/latestTS");
+    request.send();
+}
+
+/*
+    Function that requests a report based on timestamp.
+*/
+function getReportByTS(document, reportTS) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            report = JSON.parse(request.response);
+            // Gotten report. Now do something.
+            console.log("Fresh ReportID = " + report[0]["reportID"]);
+            playSound(document, report[0]["tag"]);
+        }
+    };
+    request.open("POST", "https://cruzsafe.appspot.com/api/reports/reportTS");
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.send(JSON.stringify({ reportTS: reportTS }));
+}
+
+/*
+    REPORTS PAGE CODE
+*/
+function determineSetup() {
+    const filterElements = [
+        "filterTag",
+        "filterIncidentID",
+        "filterDate",
+        "filterTime",
+        "filterStatus",
+        "filterBody",
+        "filterLocation",
+        "filterCoordinates",
+        "filterUnchangedLocation",
+        "filterAttachments",
+        "filterMobileID",
+        "filterFirstName",
+        "filterLastName",
+        "filterEmail",
+        "filterPhone",
+        "filterExpire"
+    ];
+    var filtersSet = false;
+    for (var i = 0; i < filterElements.length; i++) {
+        if (document.getElementById(filterElements[i]).value != (null | "")) {
+            filtersSet = true;
+            break;
+        }
+    }
+    // Will prevent auto-refresh of list when user is attempting to search for something
+    if (!filtersSet) {
+        setupReports(document);
+    }
+}
+
+/*
+    Function that plays a sound given a tagID.
+*/
+function playSound(document, tag) {
+    var audio = document.getElementById("incomingTag" + tag);
+    console.log("Playing audio from " + audio.id);
+    audio.play();
+}
---
+++ webApp/public/libs/reportSingle.js
@@ -0,0 +1,221 @@
+/*
+    Standardized code across reports.html and homepage.html (and potentially more.)
+    Is paired stand-alone with the Report Modal portion in both html and css files. 
+*/
+
+// Array of dictionary entries + tag ids.
+reportFields = [
+    "incidentID",
+    "resolvedUnresolved",
+    "reportTS",
+    "reportTS2",
+    "location",
+    "actualPinned",
+    "tag",
+    "fullName",
+    "mobileID",
+    "phone",
+    "email",
+    "body"
+];
+aPIKey = "AIzaSyDi4bKzq04VojQXEGXec4wDsdRVZhht5vY";
+// WebID (In the future, will be replaced by actual webID from Shibboleth!)
+const webID = 1;
+
+/*
+    generateSingleReport: takes in a specific reportID and the document itself. 
+    Assuming the report modal is in the document, makes two requests to the APIs. 
+
+    First request is for tags. Passes it to helper function.
+    Helper function requests for all information and adds to modal, then makes modal visible.
+*/
+function generateSingleReport(reportID, document) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            tags = JSON.parse(request.response);
+            // Gotten array of IDs.
+            tagDict = {};
+            Array.from(tags).forEach(function(tag) {
+                tagDict[tag["tagID"]] = tag["tagName"];
+            });
+            tagColors = {};
+            Array.from(tags).forEach(function(tag) {
+                tagColors[tag["tagName"]] = tag["color"];
+            });
+            // gotten list of all IDs. Calls generateMultipleReports for given index.
+            generateSingleReportHelper(reportID, document, tagDict, tagColors);
+        }
+    };
+    request.open("POST", "https://cruzsafe.appspot.com/api/reports/tags");
+    request.send();
+}
+function generateSingleReportHelper(reportID, document, tags, tagColors) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var productInfo = [];
+            reportInfo = JSON.parse(request.response)[0]; // Returns an array containing a single row as an object
+
+            // incidentID
+            productInfo["incidentID"] = reportInfo["incidentID"];
+            // resolved/unresolved
+            if (!!reportInfo["completeTS"]) {
+                var resolvedUnresolved = "[Complete]"; // Completed; not null
+            } else if (!!reportInfo["initialOpenTS"]) {
+                var resolvedUnresolved = "[Incomplete]"; // no complete TS but a inital open TS
+            } else {
+                // If it is a new, then it is now incomplete! Set the data in the database. Apply web ID.
+                //insertTS(1, reportID, webID);
+                var resolvedUnresolved = "[Incomplete]"; // Null
+            }
+            productInfo["resolvedUnresolved"] = resolvedUnresolved;
+            // reportTS
+            productInfo["reportTS"] = formatDate(reportInfo["reportTS"], {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                weekday: "long",
+                hour12: false
+            });
+            productInfo["reportTS2"] = formatDate(reportInfo["reportTS"], {
+                hour: "numeric",
+                minute: "numeric",
+                second: "numeric",
+                hour12: false
+            });
+            // location
+            productInfo["location"] = reportInfo["location"];
+            // actual/pinned
+            if (reportInfo["unchangedLocation"]) {
+                var actualPinned = "(Actual)";
+            } else {
+                var actualPinned = "(Pinned)";
+            }
+            productInfo["actualPinned"] = actualPinned;
+            // tag
+            tagValue = reportInfo["tag"];
+            productInfo["tag"] = tags[tagValue];
+            // fullName
+            fullName = reportInfo["lastName"] + " " + reportInfo["firstName"];
+            productInfo["fullName"] = fullName;
+            // mobileID
+            productInfo["mobileID"] = reportInfo["mobileID"];
+            // phone
+            productInfo["phone"] = reportInfo["phone"];
+            // email
+            productInfo["email"] = reportInfo["email"];
+            // body
+            productInfo["body"] = reportInfo["body"];
+
+            // All data has now been added into reportData
+            const modal = document.getElementById("report");
+
+            if (productInfo["resolvedUnresolved"].includes("New")) {
+                // For resolvedUnresolved, gets status. If resolved, green. else, red.
+                document.getElementById("resolvedUnresolved").style.color =
+                    "red";
+            } else if (
+                productInfo["resolvedUnresolved"].includes("Incomplete")
+            ) {
+                document.getElementById("resolvedUnresolved").style.color =
+                    "orange";
+            } else {
+                document.getElementById("resolvedUnresolved").style.color =
+                    "yellowgreen";
+            }
+
+            // Edit Location
+            const map = document.getElementById("reportMap");
+            map.setAttribute(
+                "src",
+                "https://www.google.com/maps/embed/v1/place?q=" +
+                    reportInfo["latitude"] +
+                    "," +
+                    reportInfo["longitude"] +
+                    "&key=" +
+                    aPIKey
+            );
+
+            // Edit photo
+            const photo = document.getElementById("reportPhoto");
+            if (reportID["attachments"]) {
+                photo.setAttribute(
+                    "src",
+                    "https://storage.googleapis.com/cruzsafe.appspot.com/" +
+                        reportID +
+                        ".jpg"
+                );
+            }
+
+            for (i = 0; i < reportFields.length; i++) {
+                // For all entries in reportFields
+                const field = reportFields[i];
+                const targetTag = document.getElementById(field);
+                targetTag.innerHTML = productInfo[field];
+            }
+            tag.setAttribute("style", "color:" + tagColors[productInfo["tag"]]);
+            document
+                .getElementById("reportResolve")
+                .setAttribute("onclick", "markComplete(" + reportID + ")"); // Add onclick function to button
+            modal.style.display = "block"; // Display the modal
+        }
+    };
+    request.open("POST", "https://cruzsafe.appspot.com/api/reports/reportID");
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.send(JSON.stringify({ id: reportID }));
+}
+
+/*
+ * Calls an API that inserts a timestamp into either comletedTS or initialOpenTS
+ */
+function insertTS(initialOpenTS, reportID, webID) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            //console.log("Updated TS.");
+        }
+    };
+    request.open("POST", "https://cruzsafe.appspot.com/api/reports/timestamp");
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.send(
+        JSON.stringify({
+            initialOpenTS: initialOpenTS,
+            reportID: reportID,
+            webID: webID
+        })
+    );
+}
+
+/*
+    Helper function that converts a JS date into readable format (styling).
+    Options are set to include hours,
+    Returns a string.
+*/
+function formatDate(mySQLDate, options) {
+    //console.log("mySQLDate = " + mySQLDate);
+    var jsDate = toDateFormat(mySQLDate);
+    jsDate = new Date(jsDate.toString());
+    //console.log(jsDate);
+    options["timeZone"] = "America/New_York"; // WHY DOES AMERICA/LOS_ANGELES NOT WORK
+    return jsDate.toLocaleString("en-US", options);
+}
+
+/*
+    The following is imported code to convert MySQL TS to JS date.
+*/
+function toDateFormat(mySQLDate) {
+    return new Date(mySQLDate.substr(0, 10) + "T" + mySQLDate.substr(11, 8));
+}
+
+function markComplete(reportID) {
+    insertTS(0, reportID, webID);
+}
+
+// A report has been selected!
+function displayReport(id) {
+    generateSingleReport(id, document); // Intializes report display
+}
+function hideReport() {
+    document.getElementById("report").style.display = "none";
+}
