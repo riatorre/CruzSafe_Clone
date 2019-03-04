3e623412e060d72eaa7c3845dfb155cef9b29f1c
Zhiyue Li <zli148@ucsc.edu>
2019-03-04 06:32:09 -0800

    Added a new screen

---
+++ mobileApp/screens/ImageView.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { SafeAreaView, Platform, Text } from "react-native";
+import { Container, Header, Left, Right, Body, Icon } from "native-base";
+import ImageViewer from "react-native-image-zoom-viewer";
+
+import styles from "../components/styles.js";
+
+class ImageView extends Component {
+    constructor(props) {
+        super(props);
+        this._isMounted = false;
+    }
+
+    componentDidMount() {
+        this._isMounted = true;
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    render() {
+        const { navigation } = this.props;
+        const image = navigation.getParam("image", "Not available");
+        const { goBack } = this.props.navigation;
+        return (
+            <SafeAreaView style={{ flex: 1 }}>
+                <Container>
+                    <Header style={styles.header_image}>
+                        <Left>
+                            <Icon
+                                name={`${
+                                    Platform.OS === "ios" ? "ios" : "md"
+                                }-arrow-back`}
+                                style={styles.icon}
+                                onPress={() => {
+                                    goBack();
+                                }}
+                            />
+                        </Left>
+                        <Body>
+                            <Text style={styles.footer_text}>Image</Text>
+                        </Body>
+                        <Right />
+                    </Header>
+                    <ImageViewer
+                        imageUrls={[{ url: image }]}
+                        backgroundColor="black"
+                    />
+                </Container>
+            </SafeAreaView>
+        );
+    }
+}
+export default ImageView;
