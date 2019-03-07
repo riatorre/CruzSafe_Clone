be2817c50be2bc8238205b85ba98ceb6d0b5c091
Kalinda Fraser <klfraser@ucsc.edu>
2019-03-07 11:28:53 -0800

    added launch tutorial button

--- mobileApp/screens/Tutorial.js
+++ mobileApp/screens/Tutorial.js
@@ -4,84 +4,104 @@
  */
 
 import React, { Component } from "react";
-import { Text, SafeAreaView, Platform } from "react-native";
+import { Text, SafeAreaView, Platform, TouchableOpacity } from "react-native";
 import {
-  Container,
-  Header,
-  Content,
-  Footer,
-  Left,
-  Right,
-  Body,
-  Icon
+    Container,
+    Header,
+    Content,
+    Footer,
+    Left,
+    Right,
+    Body,
+    Icon
 } from "native-base";
 
 import styles from "../components/styles.js";
 
 class Tutorial extends Component {
-  static navigationOptions = {
-    // Drawer Name
-    drawerLabel: "Tutorial",
-    //Drawer Icon
-    drawerIcon: ({ tintColor }) => (
-      <Icon
-        name={`${Platform.OS === "ios" ? "ios" : "md"}-help-circle-outline`}
-        style={{ fontSize: 24, color: tintColor }}
-      />
-    )
-  };
-  render() {
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <Container>
-          <Header style={styles.header}>
-            <Left>
-              {/* Icon used to open Side Drawer */}
-              <Icon
-                name={`${Platform.OS === "ios" ? "ios" : "md"}-menu`}
-                style={styles.icon}
-                onPress={() => this.props.navigation.openDrawer()}
-              />
-            </Left>
-            <Body>
-              {/* Center of Header */}
-              <Text style={styles.header_text}>Tutorial</Text>
-            </Body>
-            <Right />
-          </Header>
-          {/* Main Body */}
-          <Content contentContainerStyle={styles.container}>
-            <Text>Tutorial Goes here.</Text>
-          </Content>
-          <Footer style={styles.footer}>
-            <Left
-              style={{
-                flex: 1,
-                alignItems: "center",
-                justifyContent: "center"
-              }}
+    static navigationOptions = {
+        // Drawer Name
+        drawerLabel: "Tutorial",
+        //Drawer Icon
+        drawerIcon: ({ tintColor }) => (
+            <Icon
+                name={`${
+                    Platform.OS === "ios" ? "ios" : "md"
+                }-help-circle-outline`}
+                style={{ fontSize: 24, color: tintColor }}
             />
-            <Body
-              style={{
-                flex: 1,
-                alignItems: "center",
-                justifyContent: "center"
-              }}
-            >
-              {/* Center of Footer */}
-              <Text style={styles.footer_text}>CruzSafe</Text>
-            </Body>
-            <Right
-              style={{
-                flex: 1,
-                alignItems: "center",
-                justifyContent: "center"
-              }}
-            />
-          </Footer>
-        </Container>
-      </SafeAreaView>
-    );
-  }
+        )
+    };
+    render() {
+        return (
+            <SafeAreaView style={{ flex: 1 }}>
+                <Container>
+                    <Header style={styles.header}>
+                        <Left>
+                            {/* Icon used to open Side Drawer */}
+                            <Icon
+                                name={`${
+                                    Platform.OS === "ios" ? "ios" : "md"
+                                }-menu`}
+                                style={styles.icon}
+                                onPress={() =>
+                                    this.props.navigation.openDrawer()
+                                }
+                            />
+                        </Left>
+                        <Body>
+                            {/* Center of Header */}
+                            <Text style={styles.header_text}>Tutorial</Text>
+                        </Body>
+                        <Right />
+                    </Header>
+                    {/* Main Body */}
+                    <Content contentContainerStyle={styles.container}>
+                        <TouchableOpacity
+                            style={styles.btn}
+                            onPress={() => {
+                                console.log("launch");
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    fontSize: 18,
+                                    color: "white"
+                                }}
+                            >
+                                Launch Tutorial
+                            </Text>
+                        </TouchableOpacity>
+                    </Content>
+                    <Footer style={styles.footer}>
+                        <Left
+                            style={{
+                                flex: 1,
+                                alignItems: "center",
+                                justifyContent: "center"
+                            }}
+                        />
+                        <Body
+                            style={{
+                                flex: 1,
+                                alignItems: "center",
+                                justifyContent: "center"
+                            }}
+                        >
+                            {/* Center of Footer */}
+                            <Text style={styles.footer_text}>CruzSafe</Text>
+                        </Body>
+                        <Right
+                            style={{
+                                flex: 1,
+                                alignItems: "center",
+                                justifyContent: "center"
+                            }}
+                        />
+                    </Footer>
+                </Container>
+            </SafeAreaView>
+        );
+    }
 }
 export default Tutorial;
