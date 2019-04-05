061e03733fbd441e48ccbb56fffe42528d99eff5
Richard Torres <rtorres513@hotmail.com>
2019-04-05 15:18:01 -0700

    Start of API code reduction: messages.js complete

--- webApp/backend/DB/config.js
+++ webApp/backend/DB/config.js
@@ -47,4 +47,87 @@ connectionPool.getConnection(function(err, connection) {
     connection.release();
 });
 
+connectionPool.connectionQuery = (query, callback) => {
+    myConsole.log("[Database] Attempting to Execute Query: '" + query + "'");
+    connectionPool.getConnection(function(err, connection) {
+        if (err) {
+            myConsole.error(
+                "[Database] An error has occured retrieving a Connection"
+            );
+            myConsole.error(err);
+            callback({ message: "An Error has Occurred." });
+        } else {
+            connection.query(query, function(err, rows, fields) {
+                if (err) {
+                    myConsole.error(
+                        "[Database] An error has occured Executing Query: '" +
+                            query +
+                            "'"
+                    );
+                    myConsole.error(err);
+                    callback({ message: "An Error has occured" });
+                } else {
+                    myConsole.log(
+                        "[Database] Executed Query: '" +
+                            query +
+                            "' Successfully"
+                    );
+                    callback(rows);
+                }
+            });
+            connection.release();
+        }
+    });
+};
+
+connectionPool.verifyInt = val => {
+    return val != null && !isNaN(parseInt(val));
+};
+
+connectionPool.verifyIntArray = val => {
+    if (connectionPool.isArray(val)) {
+        for (i = 0; i < val.length; i++) {
+            if (!connectionPool.verifyInt(val[i])) {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return connectionPool.verifyInt(val);
+    }
+};
+
+connectionPool.isArray = array => {
+    return !!array && array.constructor === Array;
+};
+
+connectionPool.invalidValues = (vals, numValsExpected, callback) => {
+    if (numValsExpected === -1) {
+        myConsole.error(
+            '[Database] Invalid value(s) "' + vals + '" encountered.'
+        );
+    } else {
+        if (
+            vals === null ||
+            (connectionPool.isArray(vals) && vals.length < numValsExpected)
+        ) {
+            myConsole.error("[Database] Missing required value(s)");
+        } else if (
+            connectionPool.isArray(vals) &&
+            vals.length > numValsExpected
+        ) {
+            myConsole.error("[Database] Number of Values exceeds expected");
+        } else {
+            myConsole.error(
+                '[Database] Invalid value(s) "' + vals + '" encountered.'
+            );
+        }
+    }
+    callback();
+};
+
+connectionPool.sanitizeString = str => {
+    return mysql.escape(str);
+};
+
 module.exports = connectionPool;
--- webApp/backend/routes/messages.js
+++ webApp/backend/routes/messages.js
@@ -1,169 +1,80 @@
 const express = require("express");
 const router = express.Router();
 const connectionPool = require("../DB/config");
-const myConsole = require("../utilities/customConsole");
 
 // Default request for message with a specific ID, should return 0-1 messages
 router.post("/", function(req, res) {
-    myConsole.log(
-        "[Database] Attempting to retrieve message with messageID = " +
-            req.body.messageID
-    );
-    connectionPool.getConnection(function(err, connection) {
-        if (err) {
-            myConsole.error(
-                "[Database] An error has occured retrieving a Connection"
-            );
-            myConsole.error(err);
-            res.json({ message: "An Error has Occurred." });
-        } else {
-            connection.query(
-                "SELECT * FROM messages WHERE messageID = ?",
-                req.body.messageID,
-                function(err, rows, fields) {
-                    if (err) {
-                        myConsole.error(
-                            "[Database] An error has occured during the Query"
-                        );
-                        myConsole.log(err);
-                        res.json({ message: "An Error has Occurred." });
-                    } else {
-                        myConsole.log(
-                            "[Database] Successfully retrieved message with messageID = " +
-                                req.body.messageID
-                        );
-                        res.json(rows);
-                    }
-                }
-            );
-            connection.release();
-        }
-    });
+    if (connectionPool.verifyInt(req.body.messageID)) {
+        const query =
+            "SELECT * FROM messages WHERE messageID = " + req.body.messageID;
+        connectionPool.connectionQuery(query, val => {
+            res.json(val);
+        });
+    } else {
+        connectionPool.invalidValues(req.body.messageID, 1, () => {
+            res.json({ message: "An Error has occurred" });
+        });
+    }
 });
 
 // get all messages for a specific report, should return 0+ messages
 router.post("/getMessages", function(req, res) {
-    myConsole.log(
-        "[Database] Attempting to retrieve all messages for reportID = " +
-            req.body.reportID
-    );
-    connectionPool.getConnection(function(err, connection) {
-        if (err) {
-            myConsole.error(
-                "[Database] An error has occured retrieving a Connection"
-            );
-            myConsole.error(err);
-            res.json({ message: "An Error has Occurred." });
-        } else {
-            var query = "SELECT * FROM messages WHERE reportID = ";
-            for (i = 0; i < req.body.reportID.length; i++) {
-                if (i == 0) {
-                    query = query + req.body.reportID[i];
-                } else {
-                    query = query + " OR reportID = " + req.body.reportID[i];
-                }
+    if (connectionPool.verifyIntArray(req.body.reportID)) {
+        var query = "SELECT * FROM messages WHERE reportID = ";
+        for (i = 0; i < req.body.reportID.length; i++) {
+            if (i == 0) {
+                query = query + req.body.reportID[i];
+            } else {
+                query = query + " OR reportID = " + req.body.reportID[i];
             }
-            connection.query(query, function(err, rows, fields) {
-                if (err) {
-                    myConsole.error(
-                        "[Database] An error has occured during the Query"
-                    );
-                    myConsole.log(err);
-                    res.json({ message: "An Error has Occured." });
-                } else {
-                    myConsole.log(
-                        "[Database] Successfully retrieved all messages for reportID = " +
-                            req.body.reportID
-                    );
-                    res.json(rows);
-                }
-            });
-            connection.release();
         }
-    });
+        connectionPool.connectionQuery(query, val => {
+            res.json(val);
+        });
+    } else {
+        connectionPool.invalidValues(req.body.reportID, -1, () => {
+            res.json({ message: "An Error has occurred" });
+        });
+    }
 });
 
 // Get all messages made by webID, should return 0+ messages
 router.post("/getMessagesByUser", function(req, res) {
-    myConsole.log(
-        "[Database] Attempting to retrieve all messages by webID = " +
-            req.body.webID
-    );
-    connectionPool.getConnection(function(err, connection) {
-        if (err) {
-            myConsole.error(
-                "[Database] An error has occured retrieving a Connection"
-            );
-            myConsole.error(err);
-            res.json({ message: "An Error has Occurred." });
-        } else {
-            connection.query(
-                "SELECT * FROM messages WHERE webID = ?",
-                req.body.webID,
-                function(err, rows, fields) {
-                    if (err) {
-                        myConsole.error(
-                            "[Database] An error has occured during the Query"
-                        );
-                        myConsole.log(err);
-                        res.json({ message: "An Error has Occured." });
-                    } else {
-                        myConsole.log(
-                            "[Database] Successfully retrieved all messages by webID = " +
-                                req.body.webID
-                        );
-                        res.json(rows);
-                    }
-                }
-            );
-            connection.release();
-        }
-    });
+    if (connectionPool.verifyInt(req.body.webID)) {
+        const query = "SELECT * FROM messages WHERE webID = " + req.body.webID;
+        connectionPool.connectionQuery(query, val => {
+            res.json(val);
+        });
+    } else {
+        connectionPool.invalidValues(req.body.webID, 1, () => {
+            res.json({ message: "An Error has occurred" });
+        });
+    }
 });
 
 // Create a new message for specific report and owner, returns the messageID of new message.
 router.post("/submitMessage", function(req, res) {
-    myConsole.log(
-        "[Database] Attempting to store a new message for reportID = " +
+    if (connectionPool.verifyIntArray([req.body.reportID, req.body.webID])) {
+        const query =
+            "INSERT INTO messages (reportID, webID, messageText) VALUES (" +
             req.body.reportID +
-            " and webID = " +
-            req.body.webID
-    );
-    connectionPool.getConnection(function(err, connection) {
-        if (err) {
-            myConsole.error(
-                "[Database] An error has occured retrieving a Connection"
-            );
-            myConsole.error(err);
-            res.json({ message: "An Error has Occurred." });
-        } else {
-            const values = [
-                [req.body.reportID, req.body.webID, req.body.messageText]
-            ];
-            connection.query(
-                "INSERT INTO messages (reportID, webID, messageText) VALUES ?",
-                [values],
-                function(err, result) {
-                    if (err) {
-                        myConsole.error(
-                            "[Database] An error has occured during the Query"
-                        );
-                        myConsole.log(err);
-                        res.json({ message: "An Error has Occured." });
-                    } else {
-                        myConsole.log(
-                            "[Database] Successfully stored a new message for reportID = " +
-                                req.body.reportID +
-                                " and webID = " +
-                                req.body.webID
-                        );
-                        res.json({ messageID: result.insertId });
-                    }
-                }
-            );
-            connection.release();
-        }
-    });
+            "," +
+            req.body.webID +
+            "," +
+            connectionPool.sanitizeString(req.body.messageText) +
+            ")";
+        connectionPool.connectionQuery(query, val => {
+            res.json({ messageID: val.insertId });
+        });
+    } else {
+        connectionPool.invalidValues(
+            [req.body.webID, req.body.reportID, req.body.messageText],
+            3,
+            () => {
+                res.json({ message: "An Error has occurred" });
+            }
+        );
+    }
 });
 
 module.exports = router;
