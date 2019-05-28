12639b465876a9a7fb0adf43cf8ccf152bdfe15d
Richard Torres <rtorres513@hotmail.com>
2019-05-28 12:57:04 -0700

    Added (local) logout

--- webApp/app.js
+++ webApp/app.js
@@ -74,12 +74,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve Website Icon
-app.get("/favicon.ico", function(req, res) {
+app.get("/favicon.ico", (req, res) => {
     res.sendFile("favicon.ico", { root: __dirname + "/public" });
 });
 
 // Serve Static Resources; stylesheets, JS Libs, images,
 app.use(express.static(__dirname + "/public/static"));
+
+// caching disabled for every route
+app.use(function(req, res, next) {
+    res.set(
+        "Cache-Control",
+        "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+    );
+    next();
+});
 
 //Use the Router on the sub routes
 // User related APIs
@@ -105,38 +114,38 @@ app.get("*", (req, res, next) => {
 });
 
 // Sets default page to welcome.html
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
     res.sendFile("welcome.html", {
         root: __dirname + "/public"
     });
 });
 
 // "Redirects" '/welcome.html' to '/'; just for consistency with url and to catch before authentication check
-app.get("/welcome.html", function(req, res) {
+app.get("/welcome.html", (req, res) => {
     res.redirect("/");
 });
 
 // CSS Testing Page
-app.get("/testing.html", function(req, res) {
+app.get("/testing.html", (req, res) => {
     res.sendFile("testing.html", {
         root: __dirname + "/public"
     });
 });
 
 // CSS Grid Testing Page
-app.get("/gridTesting.html", function(req, res) {
+app.get("/gridTesting.html", (req, res) => {
     res.sendFile("gridTesting.html", {
         root: __dirname + "/public"
     });
 });
 
 // For use in serializing User into Session
-passport.serializeUser(function(user, cb) {
+passport.serializeUser((user, cb) => {
     cb(null, user);
 });
 
 // For use with getting info from User Session
-passport.deserializeUser(function(user, cb) {
+passport.deserializeUser((user, cb) => {
     cb(null, user);
 });
 
@@ -150,31 +159,6 @@ const key = fs.readFileSync("./backend/Shibboleth/sp-key.pem", "utf-8");
 // Certificate from Shibboleth IDP
 const ShibbolethCert =
     "MIIDmDCCAoACCQDLTquv7ZdiLTANBgkqhkiG9w0BAQsFADCBjTELMAkGA1UEBhMCVVMxEzARBgNVBAgMCkNhbGlmb3JuaWExEzARBgNVBAcMClNhbnRhIENydXoxLTArBgNVBAoMJFVuaXZlcnNpdHkgb2YgQ2FsaWZvcm5pYSwgU2FudGEgQ3J1ejEMMAoGA1UECwwDSVRTMRcwFQYDVQQDDA5sb2dpbi51Y3NjLmVkdTAeFw0xNTA2MDIyMzE3NDJaFw0yMDA1MzEyMzE3NDJaMIGNMQswCQYDVQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTETMBEGA1UEBwwKU2FudGEgQ3J1ejEtMCsGA1UECgwkVW5pdmVyc2l0eSBvZiBDYWxpZm9ybmlhLCBTYW50YSBDcnV6MQwwCgYDVQQLDANJVFMxFzAVBgNVBAMMDmxvZ2luLnVjc2MuZWR1MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAs8RybvRl9WBJDpXGOsoBrEfFKsAuwnz0sSFmjI4zkkV7WXuX2lLbBPhOnJdKbrXL11J6mDZgf8ydhH31nJJvP98bHo0LQIXe2t1dfLVinhtpOy3TQguQ7Biipwe4g0E+HI4U9ndL7jEO/Xf2diMGCSOKyZjSubSYFpgXj0ORpvixqsAId0R0JLA4Xf4OV8+l6BqlB7DwzzeZcf49pSq8CCP8QkgU3DzaemNM6Yvqtu1cTpYJ9HqRV2aRGj+GOOMVshmJ+8iDr02U5jh+0E+5485lNKb7gWHNA7kLa0QBWm4eBAU0DYQzgnuVG2OeHpP1mKsc77wBkfScyGlOcKvCNQIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQAZBRvJsHdjLTtoPfa8MuuL3KcyE9dj9rWHeyrnEuWuMfhO95BN6utPkneoOsDKZORHDAVELTrZ4E4iq36xLWarv+37jh2U7EFFqW4zVm/0Pmoa+NtnKTs78tF80n4+Zwt2iPSIJS0ZPHiNl2XYjNb7auwUK2XvpqBqh8rP63+nSEHmgFOzg01nWoJz2Q0uQ7C0mEV6aai0jp7M5se6pgnauX2g28ZyFORa5H0DO8Ku0SY8l9lTKRgXgsEOk8b2jJwuYnHu2dafiqrLOkdpKFPczD6ZGIx6eofqKmMeT4x+rZSvIZsq1j1wRw04gzQTHWCuEb+aN4x9ogtc8tHKC0O7";
-// Certificate from SSOCircle IDP
-const SSOCircleCert =
-    "MIIEYzCCAkugAwIBAgIDIAZmMA0GCSqGSIb3DQEBCwUAMC4xCzAJBgNVBAYTAkRFMRIwEAYDVQQKDAlTU09DaXJjbGUxCzAJBgNVBAMMAkNBMB4XDTE2MDgwMzE1MDMyM1oXDTI2MDMwNDE1MDMyM1owPTELMAkGA1UEBhMCREUxEjAQBgNVBAoTCVNTT0NpcmNsZTEaMBgGA1UEAxMRaWRwLnNzb2NpcmNsZS5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCAwWJyOYhYmWZF2TJvm1VyZccs3ZJ0TsNcoazr2pTWcY8WTRbIV9d06zYjngvWibyiylewGXcYONB106ZNUdNgrmFd5194Wsyx6bPvnjZEERny9LOfuwQaqDYeKhI6c+veXApnOfsY26u9Lqb9sga9JnCkUGRaoVrAVM3yfghv/Cg/QEg+I6SVES75tKdcLDTt/FwmAYDEBV8l52bcMDNF+JWtAuetI9/dWCBe9VTCasAr2Fxw1ZYTAiqGI9sW4kWS2ApedbqsgH3qqMlPA7tg9iKy8Yw/deEn0qQIx8GlVnQFpDgzG9k+jwBoebAYfGvMcO/BDXD2pbWTN+DvbURlAgMBAAGjezB5MAkGA1UdEwQCMAAwLAYJYIZIAYb4QgENBB8WHU9wZW5TU0wgR2VuZXJhdGVkIENlcnRpZmljYXRlMB0GA1UdDgQWBBQhAmCewE7aonAvyJfjImCRZDtccTAfBgNVHSMEGDAWgBTA1nEA+0za6ppLItkOX5yEp8cQaTANBgkqhkiG9w0BAQsFAAOCAgEAAhC5/WsF9ztJHgo+x9KV9bqVS0MmsgpG26yOAqFYwOSPmUuYmJmHgmKGjKrj1fdCINtzcBHFFBC1maGJ33lMk2bM2THx22/O93f4RFnFab7t23jRFcF0amQUOsDvltfJw7XCal8JdgPUg6TNC4Fy9XYv0OAHc3oDp3vl1Yj8/1qBg6Rc39kehmD5v8SKYmpE7yFKxDF1ol9DKDG/LvClSvnuVP0b4BWdBAA9aJSFtdNGgEvpEUqGkJ1osLVqCMvSYsUtHmapaX3hiM9RbX38jsSgsl44Rar5Ioc7KXOOZFGfEKyyUqucYpjWCOXJELAVAzp7XTvA2q55u31hO0w8Yx4uEQKlmxDuZmxpMz4EWARyjHSAuDKEW1RJvUr6+5uA9qeOKxLiKN1jo6eWAcl6Wr9MreXR9kFpS6kHllfdVSrJES4ST0uh1Jp4EYgmiyMmFCbUpKXifpsNWCLDenE3hllF0+q3wIdu+4P82RIM71n7qVgnDnK29wnLhHDat9rkC62CIbonpkVYmnReX0jze+7twRanJOMCJ+lFg16BDvBcG8u0n/wIDkHHitBI7bU1k6c6DydLQ+69h8SCo6sO9YuD+/3xAGKad4ImZ6vTwlB4zDCpu6YgQWocWRXE+VkOb+RBfvP755PUaLfL63AFVlpOnEpIio5++UjNJRuPuAA=";
-/*
-let SSOCircle = new passport_SAML(
-    {
-        protocol: "http://",
-        host: "169.233.232.43:8080",
-        path: "/login/callback",
-        issuer: "http://169.233.232.43:8080",
-        entryPoint:
-            "https://idp.ssocircle.com/sso/idpssoinit?metaAlias=%2Fpublicidp&spEntityID=http://169.233.232.43:8080",
-        cert: SSOCircleCert,
-        decryptionPvk: key,
-        privateCert: key,
-        logoutCallbackUrl: "http://169.233.232.43:8080/logout/callback"
-    },
-    function(profile, done) {
-        return done(null, profile);
-    }
-);
-
-passport.use(SSOCircle);
-
-/* */
 
 // Shibboleth SAML Strategy Definition
 let Shibboleth = new passport_SAML(
@@ -189,12 +173,37 @@ let Shibboleth = new passport_SAML(
         privateCert: key,
         logoutCallbackUrl: "https://cruzsafe.appspot.com/logout/callback"
     },
-    function(profile, done) {
+    (profile, done) => {
         return done(null, profile);
     }
 );
 
 passport.use(Shibboleth);
+
+// Function used to prepare SAML (Shibboleth) logout
+//"nameID":"ali64@ucsc.edu"
+//"nameIDFormat":"urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+passport.logoutSaml = (req, res) => {
+    //Here add the nameID and nameIDFormat to the user if you stored it someplace.
+    console.log(req.session.userCore);
+    req.user.nameID = req.session.userCore.nameID;
+    req.user.nameIDFormat = req.session.userCore.nameIDFormat;
+
+    Shibboleth.logout(req, function(err, request) {
+        if (!err) {
+            //redirect to the IdP Logout URL
+            res.redirect(request);
+        } else {
+            console.log(err);
+        }
+    });
+};
+
+// Callback function to return to welcome screen after SAML (Shibboleth) logout
+passport.logoutSamlCallback = (req, res) => {
+    req.logout();
+    res.redirect("/");
+};
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -202,11 +211,11 @@ app.use(passport.session());
 // Redirects to SAML login (Shibboleth); first portion is fine, need to fix in order to sort between Mobile and Web Requests
 app.get(
     "/login",
-    function(req, res, next) {
-        console.log(req.query);
+    (req, res, next) => {
+        //console.log(req.query);
         const redirect_Url = req.query.redirect_uri;
         req.session.redirect_Url = redirect_Url;
-        console.log(req.session.redirect_Url);
+        //console.log(req.session.redirect_Url);
         return next();
     },
     passport.authenticate("saml", {
@@ -222,14 +231,16 @@ app.post(
         failureRedirect: "/",
         failureFlash: true
     }),
-    function(req, res) {
+    (req, res) => {
         const { redirect_Url } = req.session;
         console.log(req.session);
         const { user } = req;
         const userCore = {
             firstName: user["urn:oid:2.5.4.42"],
             lastName: user["urn:oid:2.5.4.4"],
-            email: user["email"]
+            email: user["email"],
+            nameID: user["nameID"],
+            nameIDFormat: user["nameIDFormat"]
         };
         //console.log(userCore);
         let redirectUrl = "";
@@ -268,17 +279,11 @@ app.post(
             );
         } else {
             const query =
-                "INSERT INTO webUsers (firstName, lastName, email) VALUES (" +
+                "SELECT * FROM webUsers WHERE firstName = " +
                 connectionPool.sanitizeString(userCore.firstName) +
-                ", " +
+                "AND lastName = " +
                 connectionPool.sanitizeString(userCore.lastName) +
-                ", " +
-                connectionPool.sanitizeString(userCore.email) +
-                ") ON DUPLICATE KEY UPDATE webID =LAST_INSERT_ID(webID), firstName = " +
-                connectionPool.sanitizeString(userCore.firstName) +
-                ", lastName = " +
-                connectionPool.sanitizeString(userCore.lastName) +
-                ", email = " +
+                "AND email = " +
                 connectionPool.sanitizeString(userCore.email);
             connectionPool.handleAPI(
                 null,
@@ -287,23 +292,47 @@ app.post(
                 3,
                 query,
                 val => {
-                    req.session.webUserID = val.insertId;
-                    redirectUrl = "/homepage.html";
-                    req.session.userCore = userCore;
-                    res.cookie("cruzsafe_webID", val.insertId, {
-                        maxAge: 28800000,
-                        httpOnly: false,
-                        secure: true,
-                        secret: "CruzSafe_WebApp_Secret_Key"
-                    });
-                    myConsole.log("redirect: " + redirectUrl);
-                    res.redirect(redirectUrl);
+                    console.log(val[0]);
+                    if (val != null) {
+                        req.session.webUserID = val[0].webID;
+                        redirectUrl = "/homepage.html";
+                        req.session.userCore = userCore;
+                        res.cookie("cruzsafe_webID", val[0].webID, {
+                            maxAge: 28800000,
+                            httpOnly: false,
+                            secure: true,
+                            secret: "CruzSafe_WebApp_Secret_Key"
+                        });
+                        myConsole.log("redirect: " + redirectUrl);
+                        res.redirect(redirectUrl);
+                    } else {
+                        myConsole.log(
+                            "An Invalid User attempted to sign in; " +
+                                userCore.firstName +
+                                " " +
+                                userCore.lastName +
+                                "with email: " +
+                                userCore.email
+                        );
+                        res.redirect("/");
+                    }
                 },
-                () => {}
+                () => {
+                    res.redirect("/");
+                }
             );
         }
     }
 );
+
+// "https://cruzsafe.appspot.com/logout"
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
+
+// "https://cruzsafe.appspot.com/logout/callback"
+app.get("/logout/callback", passport.logoutSamlCallback);
 
 /*
 // Generate Metadata for Shibboleth
@@ -326,7 +355,7 @@ if (!localTest) {
 app.use(express.static(__dirname + "/public"));
 
 // 404 Not Found; Catch all unmatched requests
-app.get("*", function(req, res) {
+app.get("*", (req, res) => {
     res.status(404).sendFile("404.html", { root: __dirname });
 });
 
@@ -374,7 +403,7 @@ connectionPool.handleAPI(
         // Got all the buildings.
         if (buildings.length > 0 && buildings) {
             // If no, for each one, run the google map API and insert the longitude and lattitude.
-            Array.from(buildings).forEach(function(building) {
+            Array.from(buildings).forEach(building => {
                 let buildingAddress =
                     building["buildingStreet"] +
                     ", " +
@@ -388,19 +417,15 @@ connectionPool.handleAPI(
                     {
                         address: buildingAddress
                     },
-                    function(err, response) {
+                    (err, response) => {
                         if (!err) {
                             let googleResult = response.json.results;
                             console.log("Gotten google result:");
                             console.log(googleResult);
                             let buildingLat =
-                                googleResult[0]["geometry"][
-                                    "location"
-                                ]["lat"];
+                                googleResult[0]["geometry"]["location"]["lat"];
                             let buildingLng =
-                                googleResult[0]["geometry"][
-                                    "location"
-                                ]["lng"];
+                                googleResult[0]["geometry"]["location"]["lng"];
                             let buildingKey = building["buildingKey"];
 
                             // For each buildingKey, run through google maps API and insert into database.
@@ -421,7 +446,7 @@ connectionPool.handleAPI(
                                 () => {},
                                 () => {
                                     myConsole.log(
-                                            "An Error has Occurred - Updating a building's lat and long."
+                                        "An Error has Occurred - Updating a building's lat and long."
                                     );
                                 }
                             );
@@ -435,7 +460,7 @@ connectionPool.handleAPI(
     },
     () => {
         myConsole.log(
-                "An Error has occurred - Finding buildings that have null lat and long."
+            "An Error has occurred - Finding buildings that have null lat and long."
         );
     }
 );
